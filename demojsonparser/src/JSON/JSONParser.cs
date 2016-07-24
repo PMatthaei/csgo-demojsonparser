2b17fb34cf4a3b7a3d13b1bf8129af4d6391e7e0
PMatt <pmd.matthaei@gmail.com>
2016-07-24 16:12:48 +0200

    Formatting jsons

--- demojsonparser/src/JSON/JSONParser.cs
+++ demojsonparser/src/JSON/JSONParser.cs
@@ -24,7 +24,7 @@ namespace CSGO_ED.src.JSON
 
         public void dump(string s)
         {
-            //outputStream.Write(JsonConvert.SerializeObject(s, Formatting.Indented)); //Time consuming to read and display for editors
+            //outputStream.Write(JsonConvert.SerializeObject(s, Formatting.Indented)); //Time consuming to read (and display) for editors
             outputStream.Write(s);
         }
 
@@ -58,6 +58,7 @@ namespace CSGO_ED.src.JSON
 
         public string parseTick()
         {
+            //ticktime in milliseconds
             return "\"tick\": { \"tick_id\": \""+parser.CurrentTick+"\", "+"\"ticktime\": \"" + parser.CurrentTime*1000.0f + "\"";
         }
 
@@ -83,89 +84,104 @@ namespace CSGO_ED.src.JSON
             return "\"player_footstep\": {" + parsePlayer(p) + "}"; ;
         }
 
+        public string parseNade(EquipmentElement nadetype, Player thrownby, Vector position)
+        {
+            return "\"type\": \"" + nadetype.ToString() + "\", " + parsePlayer(thrownby) + ", " + parsePosition(position);
+        }
 
         #region NADES
         public string parseHegrenadeDetonated(GrenadeEventArgs he)
         {
-            return "\"hegrenade_detonated\": ";
+            return "\"hegrenade_detonated\": { " + parseNade(he.NadeType, he.ThrownBy, he.Position) + "}";
         }
 
         public string parseFlashbangDetonated(FlashEventArgs fbe)
         {
-            return "\"flashbang_detonated\": ";
+            return "\"flashbang_detonated\": {" + parseNade(fbe.NadeType, fbe.ThrownBy, fbe.Position) + "\"flashed\": {" + parsePlayers(fbe.FlashedPlayers) +"}";
         }
 
         public string parseSmokegrenadeDetonated(SmokeEventArgs se)
         {
-            return "\"smokegrenade_detonated\": ";
+            return "\"smokegrenade_detonated\": {" + parseNade(se.NadeType, se.ThrownBy, se.Position) + "}";
         }
 
         public string parseFiregrenadeDetonated(FireEventArgs fe)
         {
-            return "\"firegrenade_detonated\": ";
+            return "\"firegrenade_detonated\": {" + parseNade(fe.NadeType, fe.ThrownBy, fe.Position) + "}";
         }
 
         public string parseDecoyDetonated(DecoyEventArgs de)
         {
-            return "\"decoy_detonated\": ";
+            return "\"decoy_detonated\": {" + parseNade(de.NadeType, de.ThrownBy, de.Position) + "}";
         }
 
         public string parseFiregrenadeEnded(FireEventArgs fe)
         {
-            return "\"firegrenade_ended\": ";
+            return "\"firegrenade_ended\": {" + parseNade(fe.NadeType, fe.ThrownBy, fe.Position) + "}";
         }
 
         public string parseSmokegrenadeEnded(SmokeEventArgs se)
         {
-            return "\"smokegrenade_ended\": ";
+            return "\"smokegrenade_ended\": {" + parseNade(se.NadeType, se.ThrownBy, se.Position) + "}";
         }
 
         public string parseDecoyEnded(DecoyEventArgs de)
         {
-            return "\"decoy_ended\": ";
+            return "\"decoy_ended\":  {" + parseNade(de.NadeType, de.ThrownBy, de.Position) +  "}";
         }
 
         public string parseNadeReachedTarget(NadeEventArgs ne)
         {
-            return "\"decoy_ended\": ";
+            return "\"decoy_ended\":  {" + parseNade(ne.NadeType, ne.ThrownBy, ne.Position) + "}";
         }
         #endregion
 
+        private string parseBomb(BombEventArgs be)
+        {
+            return "\"site:\" \"" + be.Site + "\", "+ "\"player:\" \"" + be.Player.EntityID + "\"";
+        }
+
+        //more info about player than entityid? maybe detailed to tell what he got (life, armor, weapons)
+        private string parseBombDefuse(BombDefuseEventArgs bde)
+        {
+            return "\"player:\" \"" + bde.Player.EntityID + "\", " + "\"haskit:\" \"" + bde.HasKit + "\"";
+        }
+
         #region Bombevents
-        public string parseBombExploded(BombEventArgs ne)
+        public string parseBombExploded(BombEventArgs be)
         {
-            return "\"bomb_exploded\"";
+            return "\"bomb_exploded\": {" + parseBomb(be);
         }
 
 
-        public string parseBombPlanted(BombEventArgs ne)
+        public string parseBombPlanted(BombEventArgs be)
         {
-            return "\"bomb_planted\"";
+            return "\"bomb_planted\": {" + parseBomb(be);
         }
 
-        public string parseBombDefused(BombEventArgs ne) //No bombdefuseeevent??
+        public string parseBombDefused(BombEventArgs be) //No bombdefuseeevent??
         {
-            return "\"bomb_defused\"";
+            return "\"bomb_defused\": {" + parseBomb(be);
         }
 
-        public string parseBombAbortPlant(BombEventArgs ne)
+        public string parseBombAbortPlant(BombEventArgs be)
         {
-            return "\"bomb_abortplant\"";
+            return "\"bomb_abortplant\": {" + parseBomb(be);
         }
 
-        public string parseBombAbortDefuse(BombDefuseEventArgs ne)
+        public string parseBombAbortDefuse(BombDefuseEventArgs be)
         {
-            return "\"bomb_abortdefuse\"";
+            return "\"bomb_abortdefuse\": {" + parseBombDefuse(be);
         }
 
-        public string parseBombBeginPlant(BombEventArgs ne)
+        public string parseBombBeginPlant(BombEventArgs be)
         {
-            return "\"bomb_beginplant\"";
+            return "\"bomb_beginplant\": {" + parseBomb(be);
         }
 
-        public string parseBombBeginDefuse(BombDefuseEventArgs ne)
+        public string parseBombBeginDefuse(BombDefuseEventArgs be)
         {
-            return "\"bomb_begindefuse\"";
+            return "\"bomb_begindefuse\": {" + parseBombDefuse(be);
         }
         #endregion
 
@@ -192,9 +208,9 @@ namespace CSGO_ED.src.JSON
 
         public string parsePlayerMeta(Player p)
         {
-            return "\"player\": { \"player_id\": \"" + p.EntityID + "\", " + "\"playername\": \""+ p.Name + "\", \"team\": \"" + p.Team + "\"" + ", \"clan\": \"" + getClan(p)+ "\"}";
+            return "\"player\": { \"player_id\": \"" + p.EntityID + "\", " + "\"playername\": \""+ p.Name + "\", \"team\": \"" + p.Team + "\"" + ", \"clan\": \"" + getClan(p)+ "\"" + ", \"steam_id\": \"" + p.SteamID + "\"}";
         }
-
+        
         private string getClan(Player p)
         {
             switch (p.Team)
@@ -203,6 +219,14 @@ namespace CSGO_ED.src.JSON
                 case Team.Terrorist : return parser.TClanName;
                 default: return "NOCLAN";
             }
+        }
+
+        public string parsePlayers(Player[] ps)
+        {
+            string players = "";
+            foreach (var player in ps)
+                players += parsePlayerDetailed(player);
+            return players;
         }
 
         public string parsePlayer(Player p)
@@ -217,7 +241,12 @@ namespace CSGO_ED.src.JSON
 
         public string parsePlayerDetailedWithEquipment(Player p)
         {
-            return parsePlayerDetailed(p);
+            return parsePlayerDetailed(p) +", \"items\": {" + parseWeapons(p.Weapons)+"}";
+        }
+
+        public string parsePosition(Vector v)
+        {
+            return "\"position\": { \"x\": \"" + v.X + "\", " + "\"y\": \"" + v.Y + "\", " + "\"z:\" \"" + v.Z + "\"}";
         }
 
         public string parsePosition(Player player)
@@ -237,12 +266,30 @@ namespace CSGO_ED.src.JSON
 
         public string parseWeapons(IEnumerable<Equipment> wps)
         {
+            /*
+            string s = "";
+            Equipment last = wps.Last();
+            foreach (var wp in wps){
+                if(last)
+                s += parseWeapon(wp);
+            }*/
+
             string s = "";
 
-            foreach(var wp in wps){
-                s += parseWeapon(wp);
-            }
+            using (var enumerator = wps.GetEnumerator())
+            {
+                var last = !enumerator.MoveNext();
+                Equipment current;
+                while (!last)
+                {
+                    current = enumerator.Current;
+                    //process item
+                    s += parseWeapon(current);
 
+                    last = !enumerator.MoveNext();
+                    //process item extension according to flag; flag means item
+                }
+            }
             return s;
         }
 
